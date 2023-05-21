1e2a1c83c27c8660b88fac12ef479eca8842d431
Sebastien Druon <seb@homedruon.com>
2023-05-21 21:04:56 +0200

    cli interface draft

--- src/main.c
+++ src/main.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 #include <locale.h>
-#include "dict.h"
 
 int main( int argc, char** argv, char** envv ) {
 
-	dict_t* dictionnaire ;
+
 
 	setlocale(LC_ALL, "") ;
 
-	dictionnaire = build_dict_from_file( "dict.us" ) ;
 
-	printf( "coucou : %d\n", dict_search( dictionnaire, "coucou" ) ) ;
-	printf( "chienne : %d\n", dict_search( dictionnaire, "chienne" ) ) ;
-	printf( "chienn : %d\n", dict_search( dictionnaire, "chienn" ) ) ;
-	printf( "datasheet : %d\n", dict_search( dictionnaire, "datasheet" ) ) ;
 	
-	dict_free( dictionnaire ) ;
+
 	return 0 ;
 }
